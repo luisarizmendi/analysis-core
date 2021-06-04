7e65b2f4a4bfb98b3f03f5fac825f5baa61f1109
luisarizmendi <luis.ariz@gmail.com>
2021-06-04 21:58:44 +0200

    Change serializers to domain

--- src/main/java/org/larizmen/analysis/core/KafkaService.java
+++ src/main/java/org/larizmen/analysis/core/KafkaService.java
@@ -29,7 +29,7 @@ public class KafkaService {
     @Transactional
     public void orderIn(final PlaceOrderCommand placeOrderCommand) {
 
-        logger.debug("PlaceOrderCommand received: {}", placeOrderCommand);
+        logger.debug("PlaceOrderCommand received from Kafka: {}", placeOrderCommand);
         orderService.onOrderIn(placeOrderCommand);
     }
 
